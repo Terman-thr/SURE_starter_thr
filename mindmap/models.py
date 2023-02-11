41840fb490bedb281bd00bd7cd8c400085328788
HaoranTang <thr@umich.edu>
2023-02-11 09:59:39 -0500

    generate nodes with db

--- mindmap/models.py
+++ mindmap/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Node(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=200)
+    positionx = models.IntegerField()
+    positiony = models.IntegerField()
+
+
+class Relation(models.Model):
+    id = models.AutoField(primary_key=True)
+    parent = models.IntegerField()
+    child = models.IntegerField()
--- mindmap/views.py
+++ mindmap/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Node, Relation
 
 
 def get_index(request):
-    return render(request, 'index.html', {'name': 'thr'})
+    db_reset()
+    # Get all nodes
+    nodes = Node.objects.all()
+
+    # Loop through the nodes and print the contents
+    # for node in nodes:
+    #     print(node.id)
+    return render(request, 'index.html', {'nodes': nodes})
+
+
+def db_reset():
+    Node.objects.all().delete()
+    node1 = Node.objects.create(content='node 1', positionx=50, positiony=50)
+    node2 = Node.objects.create(content='node 2', positionx=360, positiony=150)
+    node3 = Node.objects.create(content='node 2', positionx=50, positiony=300)
+    node1.save()
+    node2.save()
+    node3.save()
+    nodes = Node.objects.all()
+    relation = Relation.objects.create(parent=nodes[0].id, child=nodes[1].id)
+    relation.save()
