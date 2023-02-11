ea9b943d79f4155a891f554d2772a1796a948459
HaoranTang <thr@umich.edu>
2023-02-10 20:44:25 -0500

    starter file

---
+++ SURE_starter_thr/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SURE_starter_thr project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SURE_starter_thr.settings')
+
+application = get_wsgi_application()
