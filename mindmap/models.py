ed39d59c8c21f451615e7db45696115ff51b1073
HaoranTang <thr@umich.edu>
2023-02-11 16:12:44 -0500

    api in html

--- mindmap/models.py
+++ mindmap/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Node(models.Model):
     id = models.AutoField(primary_key=True)
+    num = models.IntegerField(default=0)
     content = models.CharField(max_length=200)
     positionx = models.IntegerField()
     positiony = models.IntegerField()
